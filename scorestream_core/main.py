e58b1ed93177bf468e0ff26d4d9baa332bb58bb8
jingxin <xujingxin07@gmail.com>
2025-02-15 11:32:02 +0800

    - able to call audiveris and get xml from pdf given filepath to the pdf. - times the audivers: ~25 seconds per page. - also accesses the bar number of the last bar.

--- scorestream_core/main.py
+++ scorestream_core/main.py
@@ -1,8 +1,35 @@
 from music21 import converter, stream
+import subprocess
+from timeit import default_timer as timer
+from datetime import timedelta
 
-mxl = converter.parse("/tests/Sonate_No._14_Moonlight_1st_Movement.mxl")
+input_pdf_filepath = "C:/Users/jingx/Downloads/Polonaise.pdf"
+
+start = timer()
+
+result = subprocess.run(
+    [
+        "C:/Program Files/Audiveris/bin/Audiveris.bat",
+        "-export",
+        "-batch",
+        "-output",
+        "C:/Users/jingx/git_wa/ScoreStream-core",
+        input_pdf_filepath,
+    ],
+    # capture_output=True,
+    # text=True,
+)
+
+end = timer()
+print(timedelta(seconds=end - start))
+
+polonaise = converter.parse("../Polonaise.mxl")
+# polonaise.show("lily")
+
 stream_of_bars = (
-    mxl.flatten(retainContainers=True).getElementsByClass(stream.base.Measure).stream()
+    polonaise.flatten(retainContainers=True)
+    .getElementsByClass(stream.base.Measure)
+    .stream()
 )
 stream_of_bars.show("text")
 print(stream_of_bars[-1].number)
